c4ab98446011177e1f55f48f27fbf175b8501e30
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2022-02-14 18:17:23 -0300

    added files, redux, reducer func JS, contextAPI + REDUCER

---
+++ src/reducer.js
@@ -0,0 +1,61 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      };
+
+    case "GET_TOTALS":
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+
+      total = parseFloat(total.toFixed(2));
+
+      return { ...state, total, amount };
+
+    case "LOADING":
+      return { ...state, loading: true };
+
+    case "DISPLAY_ITEMS":
+      return { ...state, cart: action.payload, loading: false };
+
+    case "TOGGLE_AMOUNT":
+      let newCart = state.cart
+        .map((cartItem) => {
+          if (cartItem.id === action.payload.id) {
+            if (action.payload.type === "inc") {
+              return { ...cartItem, amount: cartItem.amount + 1 };
+            }
+            if (action.payload.type === "dec") {
+              return { ...cartItem, amount: cartItem.amount - 1 };
+            }
+          }
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.amount !== -1);
+
+      return { ...state, cart: newCart };
+
+    default:
+      throw new Error("no matching action type");
+  }
+};
+
+export default reducer;
